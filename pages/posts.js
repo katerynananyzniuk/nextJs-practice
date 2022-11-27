524b490555054c76561d37fdf1dbb32948356e74
Kateryna Nanyzniuk <65312887+katerynananyzniuk@users.noreply.github.com>
2022-11-27 06:47:32 +0200

    feat: added json-server

--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,23 @@
 import {MainLayout} from '../layouts/MainLayout'
+import {useState, useEffect} from 'react'
 
 export default function Posts() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch('http://localhost:8000/posts')
+      const json = await response.json()
+      setPosts(json)
+    }
+    load()
+  },[])
   return (
     <MainLayout title="Posts page">
       <h1>Posts page</h1>
+      <pre>
+        {JSON.stringify(posts,null,2)}
+      </pre>
     </MainLayout>
   )
 }
